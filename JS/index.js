9fbdd822e1cf302e6ba3a868c50d144a0facb212
babytayo <121583836+babytayo@users.noreply.github.com>
2023-04-08 13:57:30 +0100

    Changed the multiple JS written into one JS statement

--- JS/index.js
+++ JS/index.js
@@ -1,35 +1,3 @@
-//default function and functionOne
-function imgOne() {
-  document.getElementById("mainImage").src =
-    "./image/pngtreeTexturedretrocarpngillustration4614072.png";
-}
-
-//fuctionTwo
-function imgTwo() {
-  document.getElementById("mainImage").src =
-    "./image/Pngtreecoolsportscar5614767.png";
-}
-
-//functionThree
-function imgThree() {
-  document.getElementById("mainImage").src =
-    "./image/Raceway-Sprint-Car-Racing-PNG-Clipart.png";
-}
-
-//functionfour
-function imgFour() {
-  document.getElementById("mainImage").src =
-    "./image/Speedway-Sprint-Car-Racing-PNG-File.png";
-}
-
-//functionFive
-function imgFive() {
-  document.getElementById("mainImage").src =
-    "./image/Speedway-Sprint-Car-Racing-PNG-Transparent-HD-Photo.png";
-}
-
-//functionSix
-function imgSix() {
-  document.getElementById("mainImage").src =
-    "./image/Speedway-Sprint-Car-Racing-PNG.png";
+function changeImage(path) {
+  document.getElementById("mainImage").src = path;
 }
